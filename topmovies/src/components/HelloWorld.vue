9787319bc889a67f9177cfd3cc9d2c7e703acacf
Hussein-A4 <smartron4cr7@gmail.com>
2022-11-20 14:42:44 -0500

    created intial code

---
+++ topmovies/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <select v-model="selected_movie">
+    <!-- inline object literal -->
+    <option v-for="movie in movies_list" :value="{number:movie.id}">{{movie.name}}</option>
+  </select>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selected_movie: 557,
+      movies_list: [
+        {id:'557', name:'Spider-Man'},
+        {id:'558', name:'Spider-Man2'},
+        {id:'559', name:'Spider-Man'},
+        {id:'2', name:'Spider-Man2'},
+        {id:'557', name:'Spider-Man'},
+        {id:'2', name:'Spider-Man2'},
+        {id:'557', name:'Spider-Man'},
+        {id:'2', name:'Spider-Man2'},
+        {id:'557', name:'Spider-Man'},
+        {id:'2', name:'Spider-Man2'}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
