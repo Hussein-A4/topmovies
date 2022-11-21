52a6773207f42eead6a764d3d0b5d223def8791c
Hussein-A4 <smartron4cr7@gmail.com>
2022-11-20 22:00:06 -0500

    finished function and form of vue

--- topmovies/src/components/HelloWorld.vue
+++ topmovies/src/components/HelloWorld.vue
@@ -4,9 +4,14 @@
     <!-- inline object literal -->
     <option v-for="movie in movies_list" :value="movie.id">{{movie.name}}</option>
   </select>
-  <button @click="getOption()">test</button>
+  <button @click="getOption()">GET</button>
   <p>{{response_data.movie_title}}</p>
+  <p> Release Date: {{response_data.release_date}} -- Popularity: {{response_data.popularity}}</p>
+  <p> Average Rating: {{response_data.vote_average}} -- Vote Count: {{response_data.vote_count}}</p>
+  <p> Language: {{response_data.original_language}} -- Budget: ${{response_data.budget}}</p>
+  <p>Overview: {{response_data.overview}}</p>
   <img :src=response_data.img alt="">
+  <iframe :src=response_data.trailer frameborder="0" class="movieFrame"></iframe>
 </div>
 </template>
 
@@ -16,7 +21,7 @@ export default {
   data() {
     return {
       selected_movie: null,
-      response_data:
+      response_data:{
         movie_title:'',
         release_date:'',
         popularity:'',
@@ -51,22 +56,53 @@ export default {
       }
       }).then((movieData) => {
         console.log(movieData);
-        this.response_data.movie_title = movieData.data.original_title;
-        this.response_data.release_date = movieData.data.release_date;
-        this.response_data.popularity = movieData.data.popularity;
-        this.response_data.vote_average = movieData.data.vote_average;
-        this.response_data.vote_count = movieData.data.vote_count;
-        this.response_data.language = movieData.data.language;
-        this.response_data.budget = movieData.data.budget;
-        this.response_data.overview = movieData.data.overview;
-
+        let data =movieData.data
+        const trailers = data.videos.results.filter((trailer) => trailer.type === "Trailer");
+        this.response_data.movie_title = data.original_title;
+        this.response_data.release_date = data.release_date;
+        this.response_data.popularity = data.popularity;
+        this.response_data.vote_average = data.vote_average;
+        this.response_data.vote_count = data.vote_count;
+        this.response_data.language = data.original_language;
+        this.response_data.budget = data.budget;
+        this.response_data.overview = data.overview;
+        this.response_data.img = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
+        this.response_data.trailer = `https://www.youtube.com/embed/${trailers.at(0).key}`
       })
-// this.response_data to get info
     }
   }
+}
 
 
 </script>
 
 <style>
+
+img {
+    width: 400px;
+    height: 600px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 5px;
+  }
+  
+  p {
+    color: blue;
+    font-weight: bold;
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    width: 500px;
+  }
+  
+  iframe {
+    height: 600px;
+    aspect-ratio: 16 / 9;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  
+  body {
+    background-color: rgb(244, 137, 137);
+  }
+
 </style>
